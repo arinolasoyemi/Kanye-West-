0905362dd6f1942e4f9738e91e61ab6ee59406fd
arinolasoyemi <53236370+arinolasoyemi@users.noreply.github.com>
2019-07-24 14:14:54 -0400

    Add files via upload

---
+++ javawebsite.js
@@ -0,0 +1,18 @@
+function changeButtonText(){
+  var button = document.getElementById('click')
+  button.innerHTML= "Heres more about the album..."
+}
+function enlarge(x){
+  x.style.height= "400px";
+}
+function normal(x){
+  x.style.height= "200px";
+}
+// function enlarge() {
+//   document.getElementId("pics").style.height="400px";
+//   document.getElementId("pics").style.height="400px";
+// }
+
+
+// function normal()
+// window.alert("normal function")
